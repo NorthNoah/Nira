139c055495721c02e33b08580e1735ab4190613b
NorthNoah <1264918006@qq.com>
2023-02-07 21:15:27 +0800

    Revert "fix"
    This reverts commit 53256e0e7fd73d21276c49a38709bfdddeb2e7e7.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
-import React, { memo } from 'react'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-const App = memo(() => {
-  return <div>App</div>
-})
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
-export default App
+export default App;
