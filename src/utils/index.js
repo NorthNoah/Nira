89075649f1e4ef2002a19904d1b92f44aedf050c
NorthNoah <1264918006@qq.com>
2023-02-14 23:33:48 +0800

    perf: debounce优化

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 // 防止value为0被误删
 export const isFalsy = (value) => (value === 0 ? false : !value)
@@ -23,4 +23,30 @@ export const useMount = (callback) => {
   useEffect(() => {
     callback()
   }, [])
+}
+
+// 手写debounce
+// const debounce = (func, delay) => {
+//   let timeout
+//   return (...param) => {
+//     if (timeout) {
+//       clearTimeout(timeout)
+//     }
+//     timeout = setTimeout(function() {
+//       func(...param)
+//     }, delay)
+//   }
+// }
+
+export const useDebounce = (value, delay) => {
+  const [debouncedVal, setDebouncedVal] = useState(value)
+
+  useEffect(() => {
+    // 每次value变化,都会设置一个定时器
+    const timeout = setTimeout(() => setDebouncedVal(value), delay) //宏任务
+    // useEffect每执行一次,都会立即清理上次的计时器
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  return debouncedVal
 }
