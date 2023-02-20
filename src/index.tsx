b36b8b87a009df963c42e665c52997b5b34257f0
NorthNoah <1264918006@qq.com>
2023-02-20 23:29:01 +0800

    style: antd表格、输入框组件应用

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import { loadServer, DevTools } from 'jira-dev-tool'
 import { AppProviders } from 'context'
-
+import 'antd/dist/antd.less'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-loadServer(() =>
-  root.render(
-    <React.StrictMode>
-      <AppProviders>
-        <App />
-      </AppProviders>
-    </React.StrictMode>
-  )
+root.render(
+  <React.StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </React.StrictMode>
 )
--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,5 +1,6 @@
 import { useAuth } from 'context/auth-context'
 import React, { FormEvent, memo } from 'react'
+import { Button, Form, Input } from 'antd'
 const LoginPage = memo(() => {
   // const login = (param: { username: string; password: string }) => {
   //   fetch(`${apiUrl}/register`, {
@@ -14,26 +15,30 @@ const LoginPage = memo(() => {
   //   })
   // }
   const { login, user } = useAuth()
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    //查看onSumit的函数签名，判断event的类型
-    event.preventDefault()
-    const username = (event.currentTarget.elements[0] as HTMLInputElement).value //类型断言为HTMLInputElement,否则会找不到value
-    const password = (event.currentTarget.elements[1] as HTMLInputElement).value //类型断言为HTMLInputElement,否则会找不到value
-    login({ username, password })
+  // 手动实现逻辑
+  // const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+  //   //查看onSumit的函数签名，判断event的类型
+  //   event.preventDefault()
+  //   const username = (event.currentTarget.elements[0] as HTMLInputElement).value //类型断言为HTMLInputElement,否则会找不到value
+  //   const password = (event.currentTarget.elements[1] as HTMLInputElement).value //类型断言为HTMLInputElement,否则会找不到value
+  //   login({ username, password })
+  // }
+  const handleSubmit = (values: { username: string; password: string }) => {
+    login(values)
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <Form onFinish={handleSubmit}>
       {/* 登录成功判断逻辑 */}
-      {user ? (
+      {/* {user ? (
         <div>
           登录成功,用户名: {user?.name} token: {user?.token}
         </div>
-      ) : null}
-      <div>
-        <label htmlFor="username">用户名</label>
-        <input type="text" id="username" />
-      </div>
+      ) : null} */}
+      {/* 每一行用Form.item包裹 */}
+      <Form.Item name={'username'} rules={[{ required: true, message: '请输入用户名' }]}>
+        <Input placeholder={'用户名'} type="text" id={'username'} />
+      </Form.Item>
       <div>
         <label htmlFor="password">密码</label>
         <input type="password" id="password" />
@@ -41,7 +46,7 @@ const LoginPage = memo(() => {
       <div>
         <button type="submit">登录</button>
       </div>
-    </form>
+    </Form>
   )
 })
 
