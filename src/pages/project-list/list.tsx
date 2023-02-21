f80596524ad7a80f9f47940acb046fce829ebb2f
NorthNoah <1264918006@qq.com>
2023-02-21 09:27:10 +0800

    fix: 表格问题修复

--- src/pages/project-list/list.tsx
+++ src/pages/project-list/list.tsx
@@ -16,44 +16,45 @@ interface ListProps {
 }
 
 const list = memo(({ list, users }: ListProps) => {
-  const columns = [
-    {
-      title: '名称',
-      dataIndex: 'name',
-      key: 'name',
-      sorter: (a: Project, b: Project) => a.name.localeCompare(b.name)
-    }
-    // {
-    //   title: "负责人",
-    //   render: (list.map((project)) {
-    //     return <span>
-    //       {users.find((user) => user.id === project.personId)?.name || '未知'}
-    //     </span>
-    //   }
-    // }
-  ]
   return (
-    <div>
-      <Table dataSource={list} columns={columns}>
-        <thead>
-          <tr>
-            <th>名称</th>
-            <th>负责人</th>
-          </tr>
-        </thead>
-        <tbody>
-          {list.map((project) => {
-            return (
-              <tr key={project.id}>
-                <td>{project.name}</td>
-                {/* 需要从userNames中寻找到符合id的并返回 */}
-                <td>{users.find((user) => user.id === project.personId)?.name || '未知'}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </Table>
-    </div>
+    // <div>
+    //   <Table dataSource={list} columns={columns}>
+    //     <thead>
+    //       <tr>
+    //         <th>名称</th>
+    //         <th>负责人</th>
+    //       </tr>
+    //     </thead>
+    //     <tbody>
+    //       {list.map((project) => {
+    //         return (
+    //           <tr key={project.id}>
+    //             <td>{project.name}</td>
+    //             {/* 需要从userNames中寻找到符合id的并返回 */}
+    //             <td>{users.find((user) => user.id === project.personId)?.name || '未知'}</td>
+    //           </tr>
+    //         )
+    //       })}
+    //     </tbody>
+    //   </Table>
+    // </div>
+    <Table
+      pagination={false}
+      columns={[
+        {
+          title: '名称',
+          dataIndex: 'name',
+          sorter: (a, b) => a.name.localeCompare(b.name)
+        },
+        {
+          title: '负责人',
+          render(value, project) {
+            return <span>{users.find((user) => user.id === project.personId)?.name || '未知'}</span>
+          }
+        }
+      ]}
+      dataSource={list}
+    />
   )
 })
 
