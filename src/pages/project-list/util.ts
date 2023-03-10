d4768dbc3add70acf2f3846830d12866b61f21af
NorthNoah <1264918006@qq.com>
2023-03-10 16:40:50 +0800

    fix: 修复url参数问题

--- src/pages/project-list/util.ts
+++ src/pages/project-list/util.ts
@@ -1,7 +1,6 @@
-import { useUrlQueryParam } from 'utils/url'
+import { useSetUrlSearchParam, useUrlQueryParam } from 'utils/url'
 import { useMemo } from 'react'
 import { useProject } from 'utils/project'
-import { useSearchParams } from 'react-router-dom'
 
 // 项目列表搜索的参数
 export const useProjectsSearchParams = () => {
@@ -20,8 +19,7 @@ export const useProjectsQueryKey = () => {
 export const useProjectModal = () => {
   const [{ projectCreate }, setProjectCreate] = useUrlQueryParam(['projectCreate'])
   const [{ editingProjectId }, setEditingProjectId] = useUrlQueryParam(['editingProjectId'])
-  const [_, setUrlParams] = useSearchParams()
-
+  const setUrlParams = useSetUrlSearchParam()
   const { data: editingProject, isLoading } = useProject(Number(editingProjectId))
   const open = () => setProjectCreate({ projectCreate: true })
   const close = () => setUrlParams({ projectCreate: '', editingProjectId: '' })
--- src/utils/url.ts
+++ src/utils/url.ts
@@ -6,7 +6,8 @@ import { cleanObject, subset } from 'utils/index'
  * 返回页面url中，指定键的参数值
  */
 export const useUrlQueryParam = <K extends string>(keys: K[]) => {
-  const [searchParams, setSearchParam] = useSearchParams()
+  const [searchParams] = useSearchParams()
+  const setSearchParams = useSetUrlSearchParam()
   const [stateKeys] = useState(keys)
   return [
     useMemo(
@@ -19,14 +20,21 @@ export const useUrlQueryParam = <K extends string>(keys: K[]) => {
 
     // 对传入的键值对进行限制
     (params: Partial<{ [key in K]: unknown }>) => {
+      return setSearchParams(params)
       // iterator
       // iterator: https://codesandbox.io/s/upbeat-wood-bum3j?file=/src/index.js
-      const o = cleanObject({
-        // 读取键值对，并且把entries转换为obj
-        ...Object.fromEntries(searchParams),
-        ...params
-      }) as URLSearchParamsInit
-      return setSearchParam(o)
     }
   ] as const
+}
+
+export const useSetUrlSearchParam = () => {
+  const [searchParams, setSearchParam] = useSearchParams()
+  return (params: { [key in string]: unknown }) => {
+    const o = cleanObject({
+      // 读取键值对，并且把entries转换为obj
+      ...Object.fromEntries(searchParams),
+      ...params
+    }) as URLSearchParamsInit
+    return setSearchParam(o)
+  }
 }
