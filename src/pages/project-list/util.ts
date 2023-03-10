3490dc40c265dbfafb404d197fb6138d457d2f13
NorthNoah <1264918006@qq.com>
2023-03-10 16:27:54 +0800

    fix: 修复创建项目窗口关闭bug

--- src/pages/project-list/util.ts
+++ src/pages/project-list/util.ts
@@ -1,6 +1,7 @@
 import { useUrlQueryParam } from 'utils/url'
 import { useMemo } from 'react'
 import { useProject } from 'utils/project'
+import { useSearchParams } from 'react-router-dom'
 
 // 项目列表搜索的参数
 export const useProjectsSearchParams = () => {
@@ -19,13 +20,11 @@ export const useProjectsQueryKey = () => {
 export const useProjectModal = () => {
   const [{ projectCreate }, setProjectCreate] = useUrlQueryParam(['projectCreate'])
   const [{ editingProjectId }, setEditingProjectId] = useUrlQueryParam(['editingProjectId'])
+  const [_, setUrlParams] = useSearchParams()
 
   const { data: editingProject, isLoading } = useProject(Number(editingProjectId))
   const open = () => setProjectCreate({ projectCreate: true })
-  const close = () => {
-    setProjectCreate({ projectCreate: undefined })
-    setEditingProjectId({ editingProjectId: undefined })
-  }
+  const close = () => setUrlParams({ projectCreate: '', editingProjectId: '' })
   const startEdit = (id: number) => setEditingProjectId({ editingProjectId: id })
 
   return {
